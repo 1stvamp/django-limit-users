884060d038b72cd2da187e1a2fc38ee3363304d2
Wesley Mason <wes@1stvamp.org>
2010-11-05 12:16:59 +0000

    Added optional cleanup of models

--- dlu/limitusers/models.py
+++ dlu/limitusers/models.py
@@ -4,18 +4,25 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
 def user_save_handler(sender, **kwargs):
-    # Make sure we create a matching UserProfile instance whenever
-    # a new User is created.
-    if kwargs['created']:
+    if not settings.get('DISABLE_USER_REGISTRATION_LIMIT', False):
+        # Make sure we create a matching UserProfile instance whenever
+        # a new User is created.
         user = kwargs['instance']
-        filters = {}
-        if settings.get('LIMIT_USERS_IGNORE_ADMIN', True):
-            filters['is_stuff'] = False
-            filters['is_superuser'] = False
-        if User.objects.filter(**filters).count() >= settings.get('MAX_USER_REGISTRATIONS'):
-            user.disabled = True
-            user.save()
-            DisabledUser.objects.create(user=user)
+        if kwargs['created']:
+            filters = {}
+            if settings.get('LIMIT_USERS_IGNORE_ADMIN', True):
+                filters['is_stuff'] = False
+                filters['is_superuser'] = False
+            if User.objects.filter(**filters).count() >= settings.get('MAX_USER_REGISTRATIONS'):
+                user.disabled = True
+                user.save()
+                DisabledUser.objects.create(user=user)
+        elif settings.get('CLEANUP_DISABLED_USER_MODELS', False):
+            if not user.disabled:
+                disabled = DisabledUser.objects.filter(user=user)
+                if disabled:
+                    disabled.delete()
+
 post_save.connect(user_save_handler, User)
 
 class DisabledUser(models.Model):
