af18b20b22c4bce5538ef2d3ce88c93f5378a7a5
mccreary <charles.r.mccreary@gmail.com>
2011-01-24 11:27:27 +0800

    Mods so this will work

--- dlu/limitusers/models.py
+++ dlu/limitusers/models.py
@@ -4,20 +4,20 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 
 def user_save_handler(sender, **kwargs):
-    if not settings.get('DISABLE_USER_REGISTRATION_LIMIT', False):
+    if not getattr(settings, 'DISABLE_USER_REGISTRATION_LIMIT', False):
         # Make sure we create a matching UserProfile instance whenever
         # a new User is created.
         user = kwargs['instance']
         if kwargs['created']:
             filters = {}
-            if settings.get('LIMIT_USERS_IGNORE_ADMIN', True):
-                filters['is_stuff'] = False
+            if getattr(settings, 'LIMIT_USERS_IGNORE_ADMIN', True):
+                filters['is_staff'] = False
                 filters['is_superuser'] = False
-            if User.objects.filter(**filters).count() >= settings.get('MAX_USER_REGISTRATIONS'):
-                user.disabled = True
-                user.save()
+            if User.objects.filter(**filters).count() > getattr(settings, 'MAX_USER_REGISTRATIONS'):
+                user.is_active = False
                 DisabledUser.objects.create(user=user)
-        elif settings.get('CLEANUP_DISABLED_USER_MODELS', False):
+            user.save()
+        elif getattr(settings, 'CLEANUP_DISABLED_USER_MODELS', False):
             if not user.disabled:
                 disabled = DisabledUser.objects.filter(user=user)
                 if disabled:
--- dlu/limitusers/templatetags/limitusers.py
+++ dlu/limitusers/templatetags/limitusers.py
@@ -5,21 +5,65 @@ from dlu.limitusers.models import User, DisabledUser
 
 register = template.Library()
 
-@register.simple_tag
-def regs_available():
+class RegsAvailableNode(template.Node):
+    def __init__(self, available, var_name = 'registrations_available'):
+        self.available = available
+        self.var_name = var_name
+
+    def render(self, context):
+        context[self.var_name] = self.available
+        return ''
+
+def regs_available(parser, token):
     filters = {}
-    if settings.get('LIMIT_USERS_IGNORE_ADMIN', True):
-        filters['is_stuff'] = False
+    if getattr(settings, 'LIMIT_USERS_IGNORE_ADMIN', True):
+        filters['is_staff'] = False
         filters['is_superuser'] = False
-    remaining = settings.get('MAX_USER_REGISTRATIONS') - User.objects.filter(**filters).count()
+    remaining = getattr(settings, 'MAX_USER_REGISTRATIONS') - User.objects.filter(**filters).count()
     if remaining < 0:
         remaining = 0
-    return remaining
+    return RegsAvailableNode(remaining)
+
+register.tag('regs_available', regs_available)
+
+class RegsAllowedNode(template.Node):
+    def __init__(self, allowed, var_name = 'registrations_allowed'):
+        self.allowed = allowed
+        self.var_name = var_name
+
+    def render(self, context):
+        context[self.var_name] = self.allowed
+        return ''
+        
+def regs_allowed(parser, token):
+    filters = {}
+    if getattr(settings, 'LIMIT_USERS_IGNORE_ADMIN', True):
+        filters['is_staff'] = False
+        filters['is_superuser'] = False
+    allowed = getattr(settings, 'MAX_USER_REGISTRATIONS')
+    return RegsAllowedNode(allowed)
+
+register.tag('regs_allowed', regs_allowed)
 
 @register.simple_tag
 def regs_disabled():
     return DisabledUser.objects.all().count()
 
-@register.simple_tag
-def active_users():
-    return Users.objects.filter(disabled=False).count()
+class ActiveUsersNode(template.Node):
+    def __init__(self, active, var_name = 'active_users'):
+        self.active = active
+        self.var_name = var_name
+
+    def render(self, context):
+        context[self.var_name] = self.active
+        return ''
+        
+def active_users(parser, token):
+    filters = {'is_active':True}
+    if getattr(settings, 'LIMIT_USERS_IGNORE_ADMIN', True):
+        filters['is_staff'] = False
+        filters['is_superuser'] = False
+    active = User.objects.filter(**filters).count()
+    return ActiveUsersNode(active)
+
+register.tag('active_users', active_users)
